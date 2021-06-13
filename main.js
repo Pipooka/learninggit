0745d5efa35cd087fe21d3b86ea415d72ee2858b
Pipooka <good_person@ukr.net>
2021-06-13 11:39:44 +0300

    js.v0.1

--- main.js
+++ main.js
@@ -1,7 +1,75 @@
-// 'use strict';
+"use strict";
 
-var x = 16;
-var y = 4;
-var z = x % y;
+// alert('Hello');
 
-console.log('[z]', z);
+// const result = confirm ("Are you here?");
+// console.log(result);
+
+// const answer = prompt("Вам есть 18?", "18");
+
+// console.log(typeof(answer));
+
+
+// const answers = [];
+
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Как ваша Фамилия?', '');
+// answers[2] = prompt('Сколько вам лет?', '');
+
+// console.log(typeof(answers));
+
+
+// const category = "toys";
+
+// console.log(`https://someurl.com/${category}/4`);
+
+// const name = "Oleh";
+// const surname = "Chernenko"
+// alert(`Привет, ${name}, ${surname}`);
+/* Задание на урок:
+
+1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
+'Сколько фильмов вы уже посмотрели?'
+
+2) Создать объект personalMovieDB и в него поместить такие свойства:
+    - count - сюда передается ответ на первый вопрос
+    - movies - в это свойство поместить пустой объект
+    - actors - тоже поместить пустой объект
+    - genres - сюда поместить пустой массив
+    - privat - в это свойство поместить boolean(логическое) значение false
+
+3) Задайте пользователю по два раза вопросы:
+    - 'Один из последних просмотренных фильмов?'
+    - 'На сколько оцените его?'
+Ответы стоит поместить в отдельные переменные
+Записать ответы в объект movies в формате: 
+    movies: {
+        'logan': '8.1'
+    }
+
+Проверить, чтобы все работало без ошибок в консоли */
+
+const numberOfFilms = prompt('Сколько фильмов вы уже просмотрели?', '');
+
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+
+      personalMovieDB.movies [a] = b;
+      personalMovieDB.movies [c] = d;
+
+
+
+
+console.log(personalMovieDB);
+
